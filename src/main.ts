6474a8806b61a38fd467a113d89bd1e875d17c9d
Nam Phan <namph.tech@gmail.com>
2025-05-24 16:29:49 +0700

    feat: install nest config module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
   const listenPort = process.env.PORT ?? 3000;
   await app.listen(listenPort);
-  console.log(`App starts running on port ${listenPort}`);
+  logger.log(`App starts running on port ${listenPort}`);
 }
 bootstrap();
