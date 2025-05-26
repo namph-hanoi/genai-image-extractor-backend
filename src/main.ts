c1bb2a25ed5f05bb635b88f021a8d9cf784f23ae
Nam Phan <namph.tech@gmail.com>
2025-05-26 11:15:47 +0700

    feat: allow CORS requests from certain origins

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,16 @@ async function bootstrap() {
   const logger = new Logger('Bootstrap');
   try {
     const app = await NestFactory.create(AppModule);
+    // Enable CORS
+    const allowedHosts = process.env.ALLOW_HOSTS
+      ? JSON.parse(process.env.ALLOW_HOSTS)
+      : [process.env.DOMAIN];
+    app.enableCors({
+      origin: allowedHosts,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    });
     app.setGlobalPrefix('api');
     app.useGlobalFilters(new AllExceptionsFilter());
     const listenPort = process.env.PORT ?? 3000;
